3142b338284063f8b77014b2e349d3d2bff51b90
Steven Schveighoffer <schveiguy@yahoo.com>
2016-01-25 20:12:29 -0500

    copyright typo

--- source/iopipe/buffer.d
+++ source/iopipe/buffer.d
@@ -1,5 +1,5 @@
 /**
-Copyright: Copyright Steven Schveighoffer 20011-.
+Copyright: Copyright Steven Schveighoffer 2011-.
 License:   Boost License 1.0. (See accompanying file LICENSE_1_0.txt or copy at
            http://www.boost.org/LICENSE_1_0.txt)
 Authors:   Steven Schveighoffer, Dmitry Olshansky
