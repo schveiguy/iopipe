b95453388cf3ce0e3091fef956a06cf05b3afbde
Steven Schveighoffer <schveiguy@yahoo.com>
2017-10-09 00:44:52 -0400

    Update byline example to use runWithEncoding.

--- examples/byline/byline.d
+++ examples/byline/byline.d
@@ -26,28 +26,6 @@ void main(string[] args)
     if(args.length > 1 && args[1] == "-nooutput")
         doOutput = false;
     auto dev = openDev(0).bufd;
-    dev.ensureElems(4);
-    switch(dev.window.detectBOM)
-    {
-    case UTFType.Unknown:
-    case UTFType.UTF8:
-        dev.processLines!(UTFType.UTF8);
-        break;
-    case UTFType.UTF16LE:
-        dev.processLines!(UTFType.UTF16LE);
-        break;
-    case UTFType.UTF16BE:
-        dev.processLines!(UTFType.UTF16BE);
-        break;
-    case UTFType.UTF32LE:
-        dev.processLines!(UTFType.UTF32LE);
-        break;
-    case UTFType.UTF32BE:
-        dev.processLines!(UTFType.UTF32BE);
-        break;
-    default:
-        assert(0);
-    }
-
+    openDev(0).bufd.runWithEncoding!processLines;
     stdout.flush();
 }
